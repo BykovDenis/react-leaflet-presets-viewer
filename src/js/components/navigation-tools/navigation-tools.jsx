44da02a54cbd63ff43089607e33ca78acbf50785
Denis Bykov <dbykov@owm.io>
2017-03-17 14:04:11 +0300

    Работа с попап окном

--- src/js/components/navigation-tools/navigation-tools.jsx
+++ src/js/components/navigation-tools/navigation-tools.jsx
@@ -5,6 +5,17 @@ import React, { Component } from 'react';
 import navTools from './navigation-tools.scss';
 
 export default class NavigationTools extends Component {
+  enabledPopupCode(e) {
+    e.preventDefault();
+    const popup = document.getElementById('popup_code');
+    if (popup) {
+      if (popup.style.display === 'none') {
+        popup.style.display = 'flex';
+      } else {
+        popup.style.display = 'none';
+      }
+    }
+  }
   render() {
     this.i = 1;
     console.log(this.i);
@@ -12,7 +23,12 @@ export default class NavigationTools extends Component {
       <div className={navTools.tools}>
         <ul className={navTools.tools__items}>
           <li className={navTools.tools__item}>
-            <a href="1" className={navTools.tools__link} title="Get Code">Get Code</a>
+            <a
+              href="1" className={navTools.tools__link} title="Get Code"
+              onClick={this.enabledPopupCode}
+            >
+              Get Code
+            </a>
           </li>
           <li className={navTools.tools__item}>
             <a href="2" className={navTools.tools__link} title="Edit in sql-viewer">
--- src/js/components/popup-code/popup-code.jsx
+++ src/js/components/popup-code/popup-code.jsx
@@ -1,19 +1,55 @@
 /**
  * Created by Denis on 16.03.2017.
  */
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import styles from './popup-code.scss';
 
 export default class PopupCode extends Component {
+  static get propTypes() {
+    return {
+      currentURL: PropTypes.string.isRequired
+    };
+  }
+  constructor(props) {
+    super(props);
+    this.getCopyCodeInBuffer = this.getCopyCodeInBuffer.bind(this);
+    this.popupCloseWindow = this.popupCloseWindow.bind(this);
+  }
+  componentWillMount() {
+    this.popupSyle = {
+      display: 'none'
+    };
+  }
+  componentWillReceiveProps() {
+  }
+  getCopyCodeInBuffer() {
+    try {
+      this.code.select();
+      document.execCommand('copy');
+    } catch (e) {
+      console.log(`Ошибка копирования ${e.errLogToConsole}`);
+    }
+  }
+  popupCloseWindow(e) {
+    e.preventDefault();
+    this.popup.style.display = 'none';
+  }
   render() {
-    this.i = 1;
-    console.log(this.i);
     return (
-      <div className={styles.popup_code}>
-        <textarea cols="80" rows="9" className={styles.popup_code_text}>
-          {this.props.currentURL}
-        </textarea>
-        <input type="button" className={styles.button} value="Copy in buffer" />
+      <div
+        id="popup_code" className={styles.popup_code}
+        style={this.popupSyle} ref={(popup) => { this.popup = popup; }}
+      >
+        <a href="1" className={styles.popup_code_close} onClick={this.popupCloseWindow} > X </a>
+        <textarea
+          id="popup_code_text" cols="80" rows="9" className={styles.popup_code_text}
+          value={this.props.currentURL} ref={(code) => { this.code = code; }}
+          defaultValue={this.props.currentURL} onChange={this.componentWillReceiveProps}
+        />
+        <input
+          type="button" className={styles.button}
+          onClick={this.getCopyCodeInBuffer} value="Copy in buffer"
+        />
       </div>
     );
   }
