81c50388ab38b878d7568cac4687a5a6c7b47abe
Denis Bykov <dbykov@owm.io>
2017-04-14 13:37:20 +0300

    Добавлена логика по отображению интервалов дат и  по-умолчанию если не указан

--- src/js/libs/base-map.js
+++ src/js/libs/base-map.js
@@ -9,10 +9,10 @@ class BaseLayerParams {
   constructor(params) {
     this.params = params;
     this.baseLayer = 'satellite';
+    this.paramMap = '';
     this.filterLayer = 'naturalColor';
-    this.paramMap = 'lnkTheBest';
     // Определяем параметры, которые будем анализировать в адресной строке
-    this.uriParams = ['basemap', 'layer', 'lat', 'lon', 'zoom', 'where'];
+    this.uriParams = ['basemap', 'layer', 'lat', 'lon', 'zoom', 'where', 'actual'];
     this.url = this.initialMechanicalURI();
     if (this.url) {
       this.setDefaultGetParams();
@@ -42,6 +42,9 @@ class BaseLayerParams {
       lat: parseFloat(this.params.lat, 10).toFixed(4),
       lon: parseFloat(this.params.lon, 10).toFixed(4),
     };
+    if (this.paramMap) {
+      uri.actual = this.paramMap;
+    }
     if (this.params.where) {
       uri.where = this.params.where;
     }
@@ -64,6 +67,8 @@ class BaseLayerParams {
     this.params.lon = parseFloat(url.getData('lon'), 10) ? parseFloat(url.getData('lon'), 10).toFixed(4) : this.params.lon;
     // устанавливаем слой фильтр
     this.filterLayer = url.getData('layer') || this.filterLayer;
+    // устанавливаем параметр интервала
+    this.paramMap = url.getData('actual') ? url.getData('actual') : this.paramMap;
   }
 
   /**
@@ -84,12 +89,72 @@ class BaseLayerParams {
       urlPresets[this.filterLayer].urlParams +
       '&appid=' + appid;
 
+    this.tileParam = {
+      // Лучшие снимки
+      lnkTheBest: {
+        name: 'lnkTheBest',
+        description: 'best',
+        param: `&order=best${this.params.where ? `&where=${this.params.where}` : ''}`,
+      },
+      // Последние за 100 дней
+      lnkLast: {
+        name: 'lnkLast',
+        description: 'last',
+        param: `&order=last${this.params.where ? `&where=${this.params.where}` : ''}`,
+      },
+      // Последние за 14 дней
+      lnkTwoWeeks: {
+        name: 'lnkActual14',
+        description: 'best,oldest14',
+        param: `&order=best&date>${processDate.getDateBeforeDay(14)}`,
+        paramTemplate: `&order=best&date>${processDate.getDateBeforeDay(14)}`,
+      },
+      // Последние за 100 дней
+      lnkActual100: {
+        name: 'lnkActual100',
+        description: 'best,oldest100',
+        param: `&order=best&date>${processDate.getDateBeforeDay(100) || ''}`,
+        paramTemplate: `&order=best&date>${processDate.getDateBeforeDay(100) || ''}`,
+      },
+      // Параметризованная отрисовка лучших снимков по параметрам даты и облачности
+      lnkTheBestWithParams: {
+        name: 'lnkTheBestWithParams',
+        description: 'best',
+        param: `&order=best&${this.params.where ? `&where=${this.params.where}` : ''}&clouds<${1}`,
+      },
+      lnkCurrentSummer: {
+        name: 'lnkCurrentSummer',
+        description: 'best',
+        param: `&order=best&where=${this.params.where ?
+          this.params.where :
+          `${processDate.getCurrentSummerDate()[0] || ''},${processDate.getCurrentSummerDate()[1] || ''}`}`
+      },
+      lnkCurrentSpring: {
+        name: 'lnkCurrentSummer',
+        description: 'best',
+        param: `&order=best&where=between(${processDate.getCurrentSpringDate()[0]},${processDate.getCurrentSpringDate()[1]})`,
+      },
+      lnkLastSummer: {
+        name: 'lnkLastSummer',
+        description: 'best',
+        param: `&order=best&where=between(${processDate.getLastSummerDate()[0]},${processDate.getLastSummerDate()[1]})`,
+      },
+    };
+
     const baseMap = [];
     baseMap.push(`${document.location.protocol}//{s}.tile.openstreetmap.org/{z}/{x}/{y}.png`);
     if (this.baseLayer === 'vector') {
       return baseMap;
     }
-    baseMap.push(this.tileURL);
+    if (!(/where/).test(urlPresets[this.filterLayer].urlParams)) {
+      this.paramMap = 'lnkTheBest';
+      this.setDefaultGetParams();
+      this.updateURIparams();
+      baseMap.push(`${this.tileURL}${this.tileParam[this.paramMap].param}`);
+    } else {
+      this.url.removeURIParamNotReloadPage('actual');
+      baseMap.push(this.tileURL);
+    }
     if (this.baseLayer === 'satellite') {
       return baseMap;
     }
